bb119350228764e3770d2629d31a086e90d850d4
Laura Galis <lgalis@redhat.com>
2021-07-13 02:55:38 -0400

    Updated tests

--- config/setupTests.js
+++ config/setupTests.js
@@ -3,6 +3,7 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
 
 configure({ adapter: new Adapter() });
+React.useLayoutEffect = React.useEffect;
 
 global.shallow = shallow;
 global.render = render;
--- src/test/components/automation-hub/hub-card.test.js
+++ src/test/components/automation-hub/hub-card.test.js
@@ -13,25 +13,26 @@ import { shallowToJson } from 'enzyme-to-json';
 import { applyReducerHash, ReducerRegistry } from '@redhat-cloud-services/frontend-components-utilities/ReducerRegistry';
 import { defaultSettings } from '../../../helpers/shared/pagination';
 import { mockApi } from '../../../helpers/shared/__mocks__/user-login';
-import { AUTOMATION_HUB_UI_API_BASE } from '../../../utilities/constants';
-
-const ComponentWrapper = ({ store, initialEntries = [ '/ansible-dashboard' ], children }) => (
-  <IntlProvider locale="en">
-    <Provider store={ store } >
-      <MemoryRouter initialEntries={ initialEntries }>
-        <IntlProvider locale="en">
-          { children }
-        </IntlProvider>
-      </MemoryRouter>
-    </Provider>
-  </IntlProvider>
-);
+import { AUTOMATION_HUB_API_BASE, AUTOMATION_HUB_BASE, AUTOMATION_HUB_UI_API_BASE } from '../../../utilities/constants';
+import * as contentCounts from '../../../components/automation-hub/content-counts';
 
 describe('<HubCard />', () => {
   let initialProps;
   let initialState;
   const middlewares = [thunk, promiseMiddleware, notificationsMiddleware()];
   let mockStore;
+
+  const ComponentWrapper = ({ store, initialEntries = [ '/ansible-dashboard' ], children }) => (
+    <IntlProvider locale="en">
+      <Provider store={ store } >
+        <MemoryRouter initialEntries={ initialEntries }>
+          <IntlProvider locale="en">
+            { children }
+          </IntlProvider>
+        </MemoryRouter>
+      </Provider>
+    </IntlProvider>
+  );
 
   beforeEach(() => {
     global.insights = {
@@ -56,7 +57,7 @@ describe('<HubCard />', () => {
   it('should render correctly', async ()=> {
     const store = mockStore(initialState);
     const registry = new ReducerRegistry({}, [ thunk, promiseMiddleware ]);
-    registry.register({ analyticsReducer: applyReducerHash(hubReducer, hubInitialState) });
+    registry.register({ hubReducer: applyReducerHash(hubReducer, hubInitialState) });
 
     let wrapper;
     await act(async () => {
@@ -70,12 +71,15 @@ describe('<HubCard />', () => {
   it('should render correctly in loading state', () => {
     const store = mockStore(initialState);
     let wrapper;
-    shallow(<ComponentWrapper store={ store }><HubCard { ...initialProps } isLoading={ true } /></ComponentWrapper>);
+    act(() => {
+      wrapper = shallow(<ComponentWrapper store={ store }><HubCard { ...initialProps } isLoading={ true }/></ComponentWrapper>);
+    });
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 
   it('should render the stats for the featured collection', async() => {
     Date.now = jest.fn(() => new Date(Date.UTC(2021, 1, 1)).valueOf());
+    const spyHubCounts = jest.spyOn(contentCounts, 'contentCounts');
     const collectionData = [
       {
         id: '101',
@@ -85,63 +89,66 @@ describe('<HubCard />', () => {
           company: 'A company'
         },
         latest_version: {
-          contents: [
-            {
-              name: 'Content 1',
-              content_type: 'module'
-            },
-            {
-              name: 'Content 2 - to skip',
-              content_type: 'doc_fragments'
-            },
-            {
-              name: 'Content 3 - to skip',
-              content_type: 'module_utils'
-            },
-            {
-              name: 'Content 4',
-              content_type: 'module'
-            },
-            {
-              name: 'Content 5',
-              content_type: 'module'
-            },
-            {
-              name: 'Content 6',
-              content_type: 'role'
-            },
-            {
-              name: 'Content 7',
-              content_type: 'role'
-            },
-            {
-              name: 'Content 8',
-              content_type: 'any'
-            },
-            {
-              name: 'Content 9',
-              content_type: 'plugin'
-            },
-            {
-              name: 'Content 10',
-              content_type: 'plugin type'
-            },
-            {
-              name: 'Content 11',
-              content_type: 'foo'
-            }
-          ]
+          metadata: {
+            contents: [
+              {
+                name: 'Content 1',
+                content_type: 'module'
+              },
+              {
+                name: 'Content 2 - to skip',
+                content_type: 'doc_fragments'
+              },
+              {
+                name: 'Content 3 - to skip',
+                content_type: 'module_utils'
+              },
+              {
+                name: 'Content 4',
+                content_type: 'module'
+              },
+              {
+                name: 'Content 5',
+                content_type: 'module'
+              },
+              {
+                name: 'Content 6',
+                content_type: 'role'
+              },
+              {
+                name: 'Content 7',
+                content_type: 'role'
+              },
+              {
+                name: 'Content 8',
+                content_type: 'any'
+              },
+              {
+                name: 'Content 9',
+                content_type: 'plugin'
+              },
+              {
+                name: 'Content 10',
+                content_type: 'plugin type'
+              },
+              {
+                name: 'Content 11',
+                content_type: 'foo'
+              }
+            ]
+          }
         }
       }];
     const hubState = { isLoading: false,
       isHubAvailable: true,
       isError: false,
       collection: {
-        data: collectionData
+        data: collectionData,
+        meta: { count: 1 }
       },
       collections: {
-        data: [],
-        meta: { ...defaultSettings }
+        data: collectionData,
+        meta: { count: 1 }
       },
       partners: {
         data: [],
@@ -155,23 +162,26 @@ describe('<HubCard />', () => {
       i18nReducer: {
         formatMessage: ({ defaultMessage }) => defaultMessage
       },
-      hubReducer: { ...hubState, isFetching: false }
+      hubReducer: { ...hubState }
     };
     mockApi
-    .onGet(`${AUTOMATION_HUB_UI_API_BASE}/collections?deprecated=false&&limit=31`)
-    .replyOnce(200, { data: collectionData, meta: {}});
+    .onGet(`${AUTOMATION_HUB_API_BASE}/collections?deprecated=false&&limit=31`)
+    .replyOnce(200, { data: [collectionData], meta: { count: 1 }});
     mockApi
     .onGet(`${AUTOMATION_HUB_UI_API_BASE}/repo/published/?deprecated=false&offset=0&limit=1`)
-    .replyOnce(200, { data: collectionData, meta: {}});
-
+    .replyOnce(200, { data: collectionData, meta: { count: 1 }});
+    mockApi
+    .onGet(`${AUTOMATION_HUB_BASE}/content/undefined-synclist/v3/collections?limit=1&deprecated=false`)
+    .replyOnce(200, { data: collectionData, meta: { count: 1 }});
+    mockApi
+    .onGet(`${AUTOMATION_HUB_API_BASE}/namespaces?limit=1`)
+    .replyOnce(200, { data: [], meta: {}});
     const store = mockStore(featuredState);
-    let  wrapper;
     await act(async () => {
-      wrapper = mount(<ComponentWrapper store={ store }><HubCard { ...initialProps } isLoading={ false }/></ComponentWrapper>);
+      mount(<ComponentWrapper store={ store }><HubCard { ...initialProps } isLoading={ false }/></ComponentWrapper>);
     });
-    await act(async () => {
-      wrapper.update();
-    });
-    expect(wrapper.find('Featured collection')).toHaveLength(1);
+    expect(spyHubCounts).toHaveBeenCalled();
+    expect(spyHubCounts).toHaveReturnedWith(
+      { contents: { module: 3, plugin: 4, role: 2 }, total_count: 9 });
   });
 });
