df6ab35f0726bd49855ebe78ac9c86557ce822f1
rvsia <r.vsia@seznam.cz>
2022-03-16 09:48:13 +0100

    Update repository
    - update dependencies
    - use webpack-proxy
    - remove test.webpack.config
    - remove unused scripts

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -3,11 +3,12 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
-  https: true,
-  hot: false,
   useFileHash: false,
   sassPrefix: '.ansible-dashboard, .ansibleDashboard',
-  ...(process.env.BETA && { deployment: 'beta/apps' })
+  deployment: process.env.BETA ? 'beta/apps' : 'apps',
+  useProxy: true,
+  appUrl: process.env.BETA ? '/beta/ansible/ansible-dashboard' : '/ansible/ansible-dashboard',
+  env: process.env.BETA ? 'stage-beta' : 'stage-stable',
 });
 
 plugins.push(
