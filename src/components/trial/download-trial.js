7941faed567801dce67fe119d6d769d64a5a64f7
rvsia <r.vsia@seznam.cz>
2022-05-02 16:21:31 +0200

    Fix download buttons in ansible trial pages

--- src/components/trial/download-trial.js
+++ src/components/trial/download-trial.js
@@ -1,12 +1,24 @@
 import { getAxiosInstance } from '../../helpers/shared/user-login';
+import downloadFile from './download-file';
 
 const downloadTrial = async (cheksum) => {
   const token = await insights.chrome.auth.getToken();
 
-  return getAxiosInstance()
-  .get(`https://api.access.redhat.com/management/v1/images/${cheksum}/download`,
-    { headers: { Authorization: `Bearer ${token}` }}
-  );
+  try {
+    const url = await getAxiosInstance()
+    .get(`https://api.access.redhat.com/management/v1/images/${cheksum}/download`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/vnd.rhsm.noredirect+json'
+        }
+      }
+    );
+
+    downloadFile(url.body.href);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export default downloadTrial;
